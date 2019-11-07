e8d3fa14ea4f019cceaee25ad841fbe3f195c230
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-07 19:45:06 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.tjoeun.a20191107_01_numberbaseballgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun setupEvents() {
+
+    }
+}
