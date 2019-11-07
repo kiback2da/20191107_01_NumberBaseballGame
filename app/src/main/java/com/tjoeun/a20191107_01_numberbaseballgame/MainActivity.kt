8d6b7c198980a86c9ba1a31ce432cc047a984532
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-07 20:07:31 +0900

    문제 출제

--- app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
@@ -2,14 +2,36 @@ package com.tjoeun.a20191107_01_numberbaseballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.random.Random
 
 class MainActivity : BaseActivity() {
+
+    var questionNumberArray = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+        createQuestion()
+    }
+
+    fun createQuestion(){
+        while (true) {
+            val number = Random.nextInt(1, 9)
+            if(!questionNumberArray.contains(number)){
+                questionNumberArray.add(number)
+            }
+
+            if(questionNumberArray.size == 3) {
+                break
+            }
+        }
+
+        for(num in questionNumberArray){
+            Log.d("로그",num.toString())
+        }
     }
 
     override fun setValues() {
